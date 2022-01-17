a6897a2762fbe0ddaf854d9d6a4e211ba6a5ab30
Cip28 <ciprianbarbu38@yahoo.com>
2022-01-17 13:45:45 +0200

    Upgrade2

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,7 @@ export default function Home(props) {
             <div className='text'>
                 <h1>Isn't it a beautiful morning to start working full of energy?</h1>
                 <p>If your answer is <span>NO</span> you should visit <a href="https://www.google.com/search?q=puppy&rlz=1C1GCEB_enRO964RO964&sxsrf=AOaemvIPSiYoXwgPMKjOfmlITNv4oeS5sA:1642062622955&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiD7Nv6p671AhVC3KQKHZgLDNcQ_AUoAXoECAEQAw&biw=1920&bih=937&dpr=1">this</a> link.</p>
-                <Link to="/archive" className='btn'>See your previous activity</Link>
+                <Link to="/archive" className='btn'>Previous activity</Link>
             </div>
         </div>
     )
